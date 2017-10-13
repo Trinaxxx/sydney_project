dd74b1eec51db1eb6639f931ba66e6c933df5190
Trinaxxx <suinan_zhang@163.com>
2017-10-13 09:06:36 +0800

    To make a movie of Jupiter with one image

---
+++ make_jup_movie1.py
@@ -0,0 +1,49 @@
+#This code aims to display the calculated positions of Jupiter in an epoch around(before and after) the observation time in a stationary image.
+
+import matplotlib.pyplot as plt
+from astropy import units as u
+from astropy.io import fits
+import math
+import numpy as np
+import glob
+import aplpy
+
+
+
+src_ra =[100.89815095,103.80388791,106.90027557,108.99799125,110.69136961,111.71078113,111.94792362,111.31254852,109.94465037,107.8908186,105.7263204,103.58421575,102.07634707,101.33688768,101.17898453]#degrees
+src_dec = [22.90967146,22.70110046,22.42367453,22.20612867,22.01800901,21.90860811,21.91100193,22.0323094,22.24774783,22.53380322,22.8003494,23.03412188,23.18469883,23.26024333,23.28716499]#degrees
+for i in range(len(src_ra)):
+	fig=aplpy.FITSFigure('/home/trina/Desktop/2_1068235576_I.fits')
+	fig.set_xaxis_coord_type('longitude')
+	fig.set_yaxis_coord_type('latitude')
+	fig.tick_labels.set_xformat("hh:mm:ss.ss")
+	fig.tick_labels.set_yformat("dd:mm:ss.ss")
+	#grayscale
+	fig.show_grayscale(pmin=5.0, pmax=99.5, invert=False, aspect='auto')
+	#colorbar
+	fig.add_colorbar()
+	#fig.colorbar.set_axis_label_text('Flux (Jy/beam)')
+	#grid=True
+	#grid_setting
+	fig.add_grid()
+	fig.grid.show()
+	fig.grid.set_xspacing(5.0)  # degrees
+	fig.grid.set_yspacing(5.0)  # degrees
+	#circles
+	if i==6:
+		fig.show_circles(src_ra[6], src_dec[6], 0.1, edgecolor='cyan', linewidth=1.0, alpha=1.0)
+		fig.save('00'+str(6)+'.png')
+		fig.close()
+	else:
+		fig.show_circles(src_ra[i], src_dec[i], 0.1, edgecolor='r', linewidth=1.0, alpha=1.0)
+		fig.save('00'+str(i)+'.png')
+		fig.close()
+#os.system('ffmpeg -r 10 -i %03d.png -b:v 50M -r 10 jup_path.mp4')
+
+
+
+'''
+c++ switch sentence
+'''
+
+
